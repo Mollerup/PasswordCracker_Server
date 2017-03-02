e8dd7e9e424bc145d54260aa5077f4fb97a4476d
Rasmus Mollerup <rasm314r@edu.easj.dk>
2017-03-02 12:27:44 +0100

    master

--- PasswordCracker_Client/Properties/AssemblyInfo.cs
+++ PasswordCracker_Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PasswordProject")]
+[assembly: AssemblyTitle("PasswordCracker_Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PasswordProject")]
+[assembly: AssemblyProduct("PasswordCracker_Client")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a46c5545-d1c0-45ac-8331-babad1e41b08")]
+[assembly: Guid("460b53d7-bb49-45bb-b8b8-abe3ed0b5613")]
 
 // Version information for an assembly consists of the following four values:
 //
