92e5fdff242ffcf4c6937224b54b86ed68ef6024
Dan <Dan@DESKTOP-LPHO7AM>
2017-03-09 10:55:45 +0100

    Added project.

--- PasswordCracker_Client/Program.cs
+++ PasswordCracker_Client/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using PasswordCracker_Client;
 using PasswordProject;
 
 namespace PasswordClient
@@ -27,7 +28,84 @@ namespace PasswordClient
             sr = new StreamReader(ns);
             sw = new StreamWriter(ns);
 
+            while (true)
+            {
+                var msg = Console.ReadLine();
+
+                if (msg == "-exit")
+                {
+                    sw.WriteLine("exit");
+                    Console.WriteLine("Shutting down...");
+                    break;
+                }
+                else if (msg == "-crack")
+                {
+                    sw.WriteLine(msg);
+                    GetPassword();
+                    Console.WriteLine("Getting passwords");
+                    GetWordListPart();
+                    Cracking cracking = new Cracking(UserInfo, WordListPart);
+                    var passwords = cracking.RunCracking();
+                    sw.WriteLine("result");
+                    sw.WriteLine(passwords);
+                }
+            }
+        }
+
+        public static void GetPassword()
+        {
+            if (UserInfo.Count == 0)
+            {
+                Console.WriteLine();
+                var passwordfile = sr.ReadLine();
+                var passfile = passwordfile.Split('_');
+
+                foreach (var v in passfile)
+                {
+                    if (!string.IsNullOrEmpty(v))
+                    {
+                        var splitted = v.Split(':');
+                        UserInfo.Add(new UserInfo(splitted[0], splitted[1]));
+                        Console.WriteLine(v);
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Passwords recieved...");
+            }
+            else
+            {
+                sr.ReadLine();
+                Console.WriteLine("You already have the password file...");
+            }
+        }
+
+        public static void GetWordListPart()
+        {
+            if (WordListPart.Count == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Recieving wordlist part. This may take a while...");
+                var wordlist = sr.ReadLine();
+                var wl = wordlist.Split('_');
+
+                foreach (var v in wl)
+                {
+                    if (!string.IsNullOrEmpty(v))
+                    {
+                        WordListPart.Add(v);
+                    }
+                }
+
+                Console.WriteLine("Wordlist recieved...");
+                Console.WriteLine("Wordlist count: " + WordListPart.Count());
+            }
+            else
+            {
+                Console.WriteLine("You already have the wordlist part...");
+            }
 
         }
+
     }
 }
--- PasswordCracker_Client/UserInfo.cs
+++ PasswordCracker_Client/UserInfo.cs
@@ -7,33 +7,30 @@ using System.Threading.Tasks;
 namespace PasswordProject
 {
     [Serializable]
-        class UserInfo
+    class UserInfo
+    {
+        public String Username { get; set; }
+        public String EntryptedPasswordBase64 { get; set; }
+        public byte[] EntryptedPassword { get; set; }
+
+        public UserInfo(String username, String entryptedPasswordBase64)
         {
-            public String Username { get; set; }
-            public String EntryptedPasswordBase64 { get; set; }
-            public byte[] EntryptedPassword { get; set; }
-
-            public UserInfo(String username, String entryptedPasswordBase64)
+            if (username == null)
             {
-                if (username == null)
-                {
-                    throw new ArgumentNullException("username");
-                }
-                if (entryptedPasswordBase64 == null)
-                {
-                    throw new ArgumentNullException("entryptedPasswordBase64");
-                }
-                Username = username;
-                EntryptedPasswordBase64 = entryptedPasswordBase64;
-                EntryptedPassword = Convert.FromBase64String(entryptedPasswordBase64);
+                throw new ArgumentNullException("username");
             }
-
-            public override string ToString()
+            if (entryptedPasswordBase64 == null)
             {
-                return Username + ":" + EntryptedPasswordBase64;
+                throw new ArgumentNullException("entryptedPasswordBase64");
             }
+            Username = username;
+            EntryptedPasswordBase64 = entryptedPasswordBase64;
+            EntryptedPassword = Convert.FromBase64String(entryptedPasswordBase64);
+        }
+
+        public override string ToString()
+        {
+            return Username + ":" + EntryptedPasswordBase64;
         }
     }
-
-}
 }
